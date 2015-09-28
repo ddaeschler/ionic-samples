bd335d8c8bd9a782ca3f0fe65daeeeb09be91c2c
David Daeschler <david.daeschler@gmail.com>
2015-09-27 22:22:56 -0500

    Change example9 to use templates

--- example9/www/js/app.js
+++ example9/www/js/app.js
@@ -19,18 +19,51 @@ angular.module('starter', ['ionic'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('view1', {
-        url: '/view1',
-        template: '<div class="padding"><h2>View 1</h2>\
-            <button class="button button-positive" ui-sref="view2">\
-            To View 2</button></div>'
+    $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
     })
-    .state('view2', {
-        url: '/view2',
-        template: '<div class="padding"><h2>View 2</h2>\
-            <button class="button button-assertive" ui-sref="view1">\
-            To View 1</button></div>'
+    .state('app', {
+        url: '/app',
+        templateUrl: 'templates/app.html',
+        controller: 'AppCtrl'
     })
 
-    $urlRouterProvider.otherwise('/view1');
+    $urlRouterProvider.otherwise('/login');
+})
+
+.controller('LoginCtrl', function($scope) {
+
+})
+
+.controller('AppCtrl', function($scope) {
+    $scope.ratingArr = [{
+        value: 1,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 2,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 3,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 4,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 5,
+        icon: 'ion-ios-star-outline'
+    }];
+
+    $scope.setRating = function(val) {
+        var rtgs = $scope.ratingArr;
+
+        for (var i = 0; i < rtgs.length; i++) {
+            if (i < val) {
+                rtgs[i].icon = 'ion-ios-star';
+            } else {
+                rtgs[i].icon = 'ion-ios-star-outline';
+            }
+        }
+    }
 })
